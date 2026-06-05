8ce7ce60f1bf8ae8da3571536c375a889b422e57
FAIAZ5 <faiazmohtasim@gmail.com>
2026-06-05 11:48:36 +1000

    initial commit: project setup with ESLint and CI

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "indent": ["error", 2]
+    }
+  }
+];
---
+++ index.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  const greeting = "Hello, " + name + "!";
+  return greeting;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+
+console.log(greet("Faiaz"));
+console.log("2 + 3 =", add(2, 3));
