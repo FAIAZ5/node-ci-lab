55094cb199e09e2ef332137ae91e3d8cdfed8d9b
FAIAZ5 <faiazmohtasim@gmail.com>
2026-06-06 04:55:54 +1000

    add jest tests for add and greet fucntions

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-function greet(name) {
+export function greet(name) {
   const greeting = "Hello, " + name + "!";
   return greeting;
 }
 
-function add(a, b) {
+export function add(a, b) {
   return a + b;
 }
 
-
 console.log(greet("Faiaz"));
 console.log("2 + 3 =", add(2, 3));
-
