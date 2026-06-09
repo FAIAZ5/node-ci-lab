e632636b76b64d2b3623d5d651fa251b1405bab2
FAIAZ5 <faiazmohtasim@gmail.com>
2026-06-09 10:41:05 +1000

    actually fix quotes in test file (double quotes confirmed)

--- index.test.js
+++ index.test.js
@@ -1,14 +1,14 @@
-import { add, greet } from './index.js';
+import { add, greet } from "./index.js";
 
-test('add adds two numbers correctly', () => {
+test("add adds two numbers correctly", () => {
   expect(add(2, 3)).toBe(5);
 });
 
-test('add works with negative numbers', () => {
+test("add works with negative numbers", () => {
   expect(add(-1, -1)).toBe(-2);
 });
 
-test('greet returns a hello message with the name', () => {
-  expect(greet('Faiaz')).toBe('Hello, Faiaz!');
+test("greet returns a hello message with the name", () => {
+  expect(greet("Faiaz")).toBe("Hello, Faiaz!");
 });
 
